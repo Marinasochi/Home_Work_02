d603768eafbeff43b7c74f2768a5580b7d8e639b
marinamyazina <marinasochi1@rambler.ru>
2022-08-23 22:38:15 +0300

    add "Изменили формат Exercise002_Random"

--- Exercise002_Random/Program.cs
+++ Exercise002_Random/Program.cs
@@ -1,33 +1,37 @@
-﻿
-// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
- int MyRandom3()
+﻿ int MyRandom3()
 {
-    Random rnd = new Random(); //
-    int answer = rnd.Next(100,1000); // случайное число в пределах диапазона
-    if (rnd.Next(0, 2) == 0)         // условие, при котором будет возвращаться
-    {                                                    // отрицательное 3-значное число
-       return -answer;                                   // возвращение отриц 3-значного числа
-    }
-    return answer;                                       //возвращение полож 3-значного числа
+ Random rnd = new Random(); //
+int answer = rnd.Next(100,1000); // случайное число в пределах диапазона
+if (rnd.Next(0, 2) == 0)         // условие, при котором будет возвращаться
+{                                                    // отрицательное 3-значное число
+    return -answer;                                   // возвращение отриц 3-значного числа
+}
+return answer;                                       //возвращение полож 3-значного числа
 }
 
 int a = 0;
 while (a < 10)
- {
- Console.WriteLine(" ");
- a++;
+{
+    Console.WriteLine();
+    a++;
 }
 
+string text = new String('*', 40);
+
 Console.WriteLine("    Задача 1");
-Console.WriteLine("***************************************");
-// Напишите программу, которая выводит случайное 
-// трёхзначное число и удаляет вторую цифру этого числа.
+Console.WriteLine(text);
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("  Напишите программу, которая выводит \r\nслучайное трёхзначное число \r\nи удаляет вторую цифру этого числа.");
+Console.ResetColor(); 
+
 int number = MyRandom3();
-Console.WriteLine($"Случайное 3-значное число: {number}");
+Console.WriteLine($"  Случайное 3-значное число: {number}");
 if (number < 0)
 {
-    Console.WriteLine($"Вторая цифра в этом числе: {-number/10%10}. Удалим ее.");
+    Console.WriteLine($"  Вторая цифра в этом числе: {-number/10%10}. Удалим ее.");
 }
-else Console.WriteLine($"Вторая цифра в этом числе: {number/10%10}. Удалим ее.");
+else Console.WriteLine($"  Вторая цифра в этом числе: {number/10%10}. Удалим ее.");
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine($"Ответ: {number/100*10 + number%10}");
-Console.WriteLine("***************************************");
+Console.ResetColor(); 
+Console.WriteLine(text);
